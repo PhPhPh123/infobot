b47b3542212cd1d0d095d8e1ac6b931a3e874116
PC1 <sga1702@yandex.ru>
2022-07-02 18:14:00 +0200

    carcass and schema in progress

---
+++ bot_main.py
@@ -0,0 +1,17 @@
+import jinja2
+import sqlite3
+import discord
+import tkinter
+import random
+
+'''
+Settings
+'''
+
+
+def bot_main_controller():
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ bot_user_info.py
@@ -0,0 +1,36 @@
+def bot_user_info_controller(*args):
+    """
+    Осуществляет вызовы функций SELECT-a и отправки сообщений ботом
+    :param args:
+    :return:
+    """
+    pass
+
+
+def db_select(*args):
+    """
+    Осуществляет SELECT запрос на основе данных переданных из контроллера
+    :param args:
+    :return:
+    """
+    pass
+
+
+def select_form(*args):
+    """
+    Формирует текст с помощью шаблонизатора jinja для передачи в db_select
+    :param args:
+    :return:
+    """
+    pass
+
+
+def info_send_message(*args):
+    """
+    Отправляет готовое сообщение в чат ботом
+    :param args:
+    :return:
+    """
+    pass
+
+
