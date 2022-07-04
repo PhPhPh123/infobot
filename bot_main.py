61756391eee53a13649ab5949977d69e80d35bfe
Продавец03 <apxujiy3@yandex.ru>
2022-07-04 18:19:10 +0200

    added sql tables and relations

--- bot_main.py
+++ bot_main.py
@@ -3,15 +3,25 @@ import sqlite3
 import discord
 import tkinter
 import random
+from discord.ext import commands
 
 '''
 Settings
 '''
+settings = {
+    'token': 'OTkyNDM4MjE1MjU0NDg3MTQz.Gzbd3D.bwujHkk9thKD_CooZ3304pZhNFSNh7HNsRjn5Q',
+    'bot': 'Infobot_wh40k',
+    'id': 992438215254487143,
+    'prefix': '!'
+}
+
+infobot = commands.Bot(command_prefix=settings['prefix'])
 
 
-def bot_main_controller():
-    pass
+@infobot.command()
+async def info(ctx, world_name):
+    await ctx.send('123')
 
 
 if __name__ == '__main__':
-    pass
+    infobot.run(settings['token'])
