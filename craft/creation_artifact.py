e49f86498c8083b387cfcb325a402e61b17acd4a
PC1 <sga1702@yandex.ru>
2022-08-19 16:55:34 +0200

    added suffix, prefix, name funcs

--- craft/creation_artifact.py
+++ craft/creation_artifact.py
@@ -9,6 +9,7 @@ def choise_class_objects(art_user_dict, cursor):
 
     art_object = None
 
+    print(art_user_dict)
     if art_user_dict['группа'] == 'броня':
         art_object = Armor(art_user_dict['грейд'], art_user_dict['тип'], cursor)
     elif art_user_dict['группа'] == 'оружие-дб':
@@ -16,34 +17,30 @@ def choise_class_objects(art_user_dict, cursor):
     elif art_user_dict['группа'] == 'оружие-бб':
         art_object = CloseCombatWeapon(art_user_dict['грейд'], art_user_dict['тип'], cursor)
     elif art_user_dict['группа'] == 'бижутерия':
-        art_object = Jewerly(art_user_dict['грейд'], cursor)
+        art_object = Jewerly(art_user_dict['грейд'], art_user_dict['тип'], cursor)
     elif art_user_dict['группа'] == 'random':
         art_object = random.choice([
             Armor(art_user_dict['грейд'], art_user_dict['тип'], cursor),
             RangeWeapon(art_user_dict['грейд'], art_user_dict['тип'], cursor),
             CloseCombatWeapon(art_user_dict['грейд'], art_user_dict['тип'], cursor),
-            Jewerly(art_user_dict['грейд'], cursor)
+            Jewerly(art_user_dict['грейд'], art_user_dict['тип'], cursor)
         ])
     else:
         'Некорректный запрос'
 
     final_string = art_object.__dict__
 
-    print(art_user_dict)
-    print(final_string)
-
     return final_string
 
 
 def count_gear_score(grade):
-    base_gear_score = 0
-    if grade == 'Зеленый'.lower():
+    if grade == 'зеленый':
         base_gear_score = 40
-    elif grade == 'Синий'.lower():
+    elif grade == 'синий':
         base_gear_score = 60
-    elif grade == 'Фиолетовый'.lower():
+    elif grade == 'фиолетовый':
         base_gear_score = 80
-    elif grade == 'Красный'.lower():
+    elif grade == 'красный':
         base_gear_score = 100
     else:
         return 'Данный грейд отсутствует'
@@ -56,32 +53,44 @@ def count_gear_score(grade):
 class Artifact:
     def __init__(self, gear_score, cursor):
         self.cursor = cursor
-        self.name = 'Артефакт по умолчанию'
+        self.name = 'Артефакт'
         self.grade = 'Зеленый'
-        self.weight = 10
-        self.unique_prefix = 'Особенности отсутствуют'
-        self.unique_suffix = self.choise_suffix(self.cursor)
+        self.weight = 1
+        self.unique_prefix = ''
+        self.unique_suffix = ''
         self.stat_requeriments = 'Требования отсутствуют'
         self.gear_score = gear_score
 
-    def choise_suffix(self, cursor):
-        suffix_tuple = list(self.cursor.execute('''
-SELECT effect_name, effect_text FROM unique_suffix
+    def choise_random_type_of_artifact(self, artifact_group):
+        chosen_artifact = tuple(self.cursor.execute(f'''
+    SELECT art_type_name FROM artifact_type
+    WHERE art_group_name == '{artifact_group}'
+    ORDER BY RANDOM()
+    LIMIT 1'''))[0][0]
+        return chosen_artifact
+
+    def choise_prefix(self):
+        prefix_tuple = tuple(self.cursor.execute('''
+SELECT * FROM unique_prefix
 ORDER BY RANDOM()
 LIMIT 1'''))
+        return prefix_tuple
+
+    def choise_suffix(self, art_type):
+        suffix_tuple = tuple(self.cursor.execute(f'''
+SELECT unique_suffix.effect_name, unique_suffix.effect_text 
+FROM unique_suffix
+INNER JOIN unique_suffix_art_type_relations ON unique_suffix.effect_name == unique_suffix_art_type_relations.effect_name
+INNER JOIN artifact_type ON unique_suffix_art_type_relations.art_type_name == artifact_type.art_type_name
+WHERE artifact_type.art_type_name == '{art_type}'
+ORDER BY RANDOM()
+LIMIT 1'''))
+        print(suffix_tuple)
+        print(art_type)
         return suffix_tuple
 
-
-class Armor(Artifact):
-    def __init__(self, gear_score, armor_type, cursor):
-        super().__init__(gear_score, cursor)
-        self.armor = 0
-        self.speed_modifier = 0
-        self.evasion_modifier = 0
-        self.armor_type = armor_type
-
-    def get_armor(self):
-        pass
+    def form_name(self, prefix, art_type, suffix):
+        self.name = f'{prefix[0][0]} {art_type} {suffix[0][0]}'
 
 
 class Weapon(Artifact):
@@ -91,33 +100,52 @@ class Weapon(Artifact):
         self.penetration = 'Отсутствует'
         self.prescision_modifier = 0
 
-    def get_damage(self, group_of_weapon, grade):
-        base_damage_lazgun = 18
-        base_damage_lazgpistol = 12
-        base_damage_autopistol = 18
-        base_damage_autogun = 30
+    # def get_damage(self, group_of_weapon, grade):
+    #     base_damage_lazgun = 18
+    #     base_damage_lazgpistol = 12
+    #     base_damage_autopistol = 18
+    #     base_damage_autogun = 30
+    #
+    #     random_modifier = random.uniform(0.9, 1.1)
+    #
+    #     grade_damage = 0
+    #     if grade == 'зеленый':
+    #         grade_damage = 1.05
+    #     elif grade_damage == 'синий':
+    #         grade_damage = 1.10
+    #     elif grade_damage == 'фиолетовый':
+    #         grade_damage = 1.15
+    #     elif grade_damage == 'красный':
+    #         grade_damage = 1.2
+    #
+    #     if group_of_weapon == 'Лазган':
+    #         self.damage = base_damage_lazgun * grade_damage * random_modifier
+    #         print(self.damage)
 
-        random_modifier = random.uniform(0.9, 1.1)
 
-        grade_damage = 0
-        if grade == 'зеленый':
-            grade_damage = 1.05
-        elif grade_damage == 'синий':
-            grade_damage = 1.10
-        elif grade_damage == 'фиолетовый':
-            grade_damage = 1.15
-        elif grade_damage == 'красный':
-            grade_damage = 1.2
+class Armor(Artifact):
+    def __init__(self, gear_score, armor_type, cursor):
+        super().__init__(gear_score, cursor)
+        self.armor = 0
+        self.speed_modifier = 0
+        self.evasion_modifier = 0
+        self.art_type = armor_type if armor_type != 'random' else self.choise_random_type_of_artifact('броня')
+        self.unique_prefix = self.choise_prefix()
+        self.unique_suffix = self.choise_suffix(self.art_type)
+        self.form_name(self.unique_prefix, self.art_type, self.unique_suffix)
 
-        if group_of_weapon == 'Лазган':
-            self.damage = base_damage_lazgun * grade_damage * random_modifier
-            print(self.damage)
+    def get_armor(self):
+        pass
 
 
 class Jewerly(Artifact):
-    def __init__(self, gear_score, cursor):
+    def __init__(self, gear_score, jewelry_type, cursor):
         super().__init__(gear_score, cursor)
         self.jewerly_bonus = 'Отсутствует'
+        self.art_type = jewelry_type if jewelry_type != 'random' else self.choise_random_type_of_artifact('бижутерия')
+        self.unique_prefix = self.choise_prefix()
+        self.unique_suffix = self.choise_suffix(self.art_type)
+        self.form_name(self.unique_prefix, self.art_type, self.unique_suffix)
 
 
 class RangeWeapon(Weapon):
@@ -125,19 +153,18 @@ class RangeWeapon(Weapon):
         super().__init__(gear_score, cursor)
         self.weapon_range = 10
         self.attack_speed = 1
-        self.weapon_type = weapon_type if weapon_type != 'random' else self.choise_range_weapon()
-
-    def choise_range_weapon(self):
-        chosen_range_weapon = tuple(self.cursor.execute('''
-SELECT art_type_name FROM artifact_type
-WHERE art_group_name == 'оружие-дб'
-ORDER BY RANDOM()
-LIMIT 1'''))[0][0]
-        return chosen_range_weapon
+        self.art_type = weapon_type if weapon_type != 'random' else self.choise_random_type_of_artifact('оружие-дб')
+        self.unique_prefix = self.choise_prefix()
+        self.unique_suffix = self.choise_suffix(self.art_type)
+        self.form_name(self.unique_prefix, self.art_type, self.unique_suffix)
 
 
 class CloseCombatWeapon(Weapon):
     def __init__(self, gear_score, weapon_type, cursor):
         super().__init__(gear_score, cursor)
         self.parry_bonus = 0
-        self.weapon_type = weapon_type
+        self.art_type = weapon_type if weapon_type != 'random' else self.choise_random_type_of_artifact('оружие-бб')
+        self.unique_prefix = self.choise_prefix()
+        self.unique_suffix = self.choise_suffix(self.art_type)
+        self.form_name(self.unique_prefix, self.art_type, self.unique_suffix)
+
